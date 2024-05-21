202faa49c28a066498d17c90c1ec519f2a975a73
dash420 <gabrielmialchi@gmail.com>
2024-05-21 17:54:12 -0300

    Quinta Aula Modulo 26
    Add Particle System

---
+++ ebac_modulo_26/Assets/Scripts/Buttons/ButtonStartGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStartGame : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    public void OnClick()
+    {
+        particle.Play();
+    }
+
+}
